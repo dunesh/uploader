053ab41e4cfc599652fe4c830c2edba5ae76feb2
dinesh <dnshshrestha@gmail.com>
2012-07-13 16:09:17 +0545

    some changes in files

--- app/controllers/uploader_controller.rb
+++ app/controllers/uploader_controller.rb
@@ -1,10 +1,14 @@
 class UploaderController < ApplicationController
   def upload
   @photo=Upload.new()
+  
   end
   
   def create
+ 
+  @photo=Upload.new(params[:name=>params[:id].original_filename])
+    @photo.save!
+  redirect_to :back
   
-  binding.pry
   end
 end
